ef0b05506c52987bcb3524d4a2712f35da4479fb
Maha-Rabah <maharabah13@gmail.com>
2024-05-18 05:56:21 +0300

    last edit

--- src/modules/main/components/contribution/contribution.jsx
+++ src/modules/main/components/contribution/contribution.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import './contributionStyle.css'
-import roze from '../../../../assets/roze.svg'
 import yellow from '../../../../assets/yellow.svg'
 const Contribution = ({conticard}) => {
   return (
     <div className='contribution'>
         <h2 className='title'>Our Contribution to SDGs</h2>
          <div className="container">
-      <img src={roze} alt="" className='roze1' />
       {conticard.map(function (item) {
                 return (
                   <>
